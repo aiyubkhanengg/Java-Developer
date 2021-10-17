abf98c38aac37f55973d2be789711d07e4f8c6f8
MOHD AIYUB KHAN <92619125+aiyubkhanengg@users.noreply.github.com>
2021-10-17 11:01:12 +0530

    Multiplication of Three Numbers
    Function to find the multiplication of 3 numbers in Java

---
+++ MultiOfthreeNumber.java
@@ -0,0 +1,12 @@
+//Function to find the Multiplication of 3 numbers
+//This Program Coded By AIYUB KHAN GIT (aiyubkhanengg/Java-Developer)
+
+public class MultiOfthreeNumber
+{  
+public static void main(String args[])   
+{  
+int n1 = 20, n2 = 30, n3 = 40, multiply;  
+multiply = n1 * n2 * n3;  
+System.out.println("The Multiplication Of Three Number Is: "+multiply);  
+}  
+}  
