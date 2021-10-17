50834f334e8b020f4873298f5943972407a83cfb
MOHD AIYUB KHAN <92619125+aiyubkhanengg@users.noreply.github.com>
2021-10-17 10:57:42 +0530

    Sum Of Three Number
    Function to find the sum of 3 numbers

---
+++ SumOfThreeNumber.java
@@ -0,0 +1,12 @@
+//Function to find the sum of 3 numbers
+// This program Coded By AIYUB KHAN GIT(aiyubkhanengg/Java-Developer)
+
+public class SumOfThreeNumber
+{  
+public static void main(String args[])   
+{  
+int n1 = 100, n2 = 110, n3 = 50, sum;  
+sum = n1 + n2 + n3;  
+System.out.println("The Sum Of Two Number Is: "+sum);  
+}  
+}    
