52ffa9ca1d4ffe49bfa49b38eb10f7cbe4cd7a2d
MOHD AIYUB KHAN <92619125+aiyubkhanengg@users.noreply.github.com>
2021-10-17 10:59:39 +0530

    Multiplication Of Two Number
    Function to find the Multiplication of 2 numbers in java

---
+++ MultiOftwoNumber.java
@@ -0,0 +1,12 @@
+//Function to find the Multiplication of 2 numbers
+//This Program Coded By AIYUB KHAN GIT (aiyubkhanengg/Java-Developer)
+
+public class MultiOftwoNumber
+{  
+public static void main(String args[])   
+{  
+int n1 = 20, n2 = 30, multiply;  
+multiply = n1 * n2;  
+System.out.println("The Multiplication Of Two Number Is: "+multiply);  
+}  
+}  
