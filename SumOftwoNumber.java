b8dbe374ad4ee6a486c79a142c10b0fd612c877d
MOHD AIYUB KHAN <92619125+aiyubkhanengg@users.noreply.github.com>
2021-10-17 10:53:11 +0530

    Sum Of Two Number
    Function to find the sum of 2 numbers

---
+++ SumOftwoNumber.java
@@ -0,0 +1,9 @@
+public class SumOftwoNumber
+{  
+public static void main(String args[])   
+{  
+int n1 = 100, n2 = 110, sum;  
+sum = n1 + n2;  
+System.out.println("The Sum Of Two Number Is: "+sum);  
+}  
+}  
