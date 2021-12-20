c5ea61a01da6d55ed81ab7a1285bf76c67f10645
MOHD AIYUB KHAN <92619125+aiyubkhanengg@users.noreply.github.com>
2021-12-20 20:04:57 +0530

    StringFirstNonRepeatedCharacterStream.java
    Given a String, find the first non-repeated character in it using Stream functions?

---
+++ stringFirstNonRepeatedCharacterStream.java
@@ -0,0 +1,24 @@
+//Given a String, find the first non-repeated character in it using Stream functions?
+//This Program Coded By AIYUB KHAN GIT (aiyubkhanengg/Java-Developer)
+//@uther MOHD AIYUB KHAN
+
+import java.util.*;
+import java.util.stream.*;
+import java.util.function.Function;
+
+public class stringFirstNonRepeatedCharacterStream {
+    public static void main(String args[]) {
+            String input = "Aiyub Khan Best Java Developer";
+
+            Character result = input.chars() // Stream of String       
+                                    .mapToObj(s -> Character.toLowerCase(Character.valueOf((char) s))) // First convert to Character object and then to lowercase         
+                                    .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())) //Store the chars in map with count 
+                                    .entrySet()
+                                    .stream()
+                                    .filter(entry -> entry.getValue() == 1L)
+                                    .map(entry -> entry.getKey())
+                                    .findFirst()
+                                    .get();
+            System.out.println(result);                    
+    }
+}
